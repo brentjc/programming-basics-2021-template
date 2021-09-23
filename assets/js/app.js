3020ed79e2a9a91b9683f04a939b6d538b2cca11
brentjc <crom0009@hz.nl>
2021-09-23 18:30:02 +0200

    updated assesment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-console.log('JavaScript is working!')
+let firstName= 'Brent';
+let lastName= 'Crombeen';
+let age= '17';
+let myStr= 'Hello, I am '+firstName+' '+lastName+' and I am '+age+' years old.';
+console.log(myStr);
+
+priceOfAnApple = 0.59;
+numberOfApples = 200;
+let totalCost = priceOfAnApple * numberOfApples;
+console.log(totalCost);
+const a = 5, b = 6;
+let c;
+c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+console.log(c)
+let thisStr = 'Please visit the HZ University of Applied Sciences'
+thisStr = 'Please visit the HZ University of Mad Sciences'
+console.log(thisStr)
---
+++ assets/js/assesment.js
@@ -0,0 +1,32 @@
+// Even or odd number
+if ( a % 2 == 0)
+{console.log('Even Number');}
+else {console.log('Odd Number');}
+
+//String Expression
+let textStr = 'Programming is not so cool';
+textStr = 'Programming is so cool';
+console.log(textStr)
+
+//Bouncing balls
+let colour = "orange";
+const radius = 10;
+let xVelocity = 5;
+let yVelocity = 6;
+
+createNewBall(radius, colour, xVelocity, yVelocity);
+
+let colour = "black";
+const radius = 10;
+let xVelocity = 5;
+let yVelocity = 6;
+
+createNewBall(radius, colour, xVelocity, yVelocity);
+
+let colour = "red";
+const radius = 10;
+let xVelocity = 5;
+let yVelocity = 6;
+
+createNewBall(radius, colour, xVelocity, yVelocity);
+
